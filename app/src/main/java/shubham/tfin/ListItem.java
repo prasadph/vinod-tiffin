f2c528c7d0b8aaa7c94410126bd362aa9c3fe095
Shubham Naik <shubham.naik40@gmail.com>
2015-04-26 20:29:24 +0530

    Added: ListView and ListView Adapter for First Screen

---
+++ app/src/main/java/shubham/tfin/ListItem.java
@@ -0,0 +1,22 @@
+package shubham.tfin;
+
+/**
+ * Created by shubham on 4/26/15.
+ */
+public class ListItem {
+    String dishName;
+    double rate;
+    int img;
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public int getImg() {
+        return img;
+    }
+}
--- app/src/main/java/shubham/tfin/MainActivity.java
+++ app/src/main/java/shubham/tfin/MainActivity.java
@@ -1,17 +1,34 @@
 package shubham.tfin;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    ListView lView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lView = (ListView) findViewById(R.id.lView);
+        ArrayList<ListItem> al = new ArrayList<ListItem>();
+        for(int i=0;i<4;i++)
+        {
+            ListItem lm = new ListItem();
+            lm.dishName = "TFin Dish Name";
+            lm.rate = 200.9;
+            lm.img = R.mipmap.ic_launcher;
+            al.add(lm);
+        }
+        lView.setAdapter(new ListAdapter(al,this));
     }
 
 
